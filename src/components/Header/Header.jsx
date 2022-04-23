89682a4873a3a935e5a1f642c6d20d6da1565775
aov-webd <a.ovchinnikov@itpdevelopment.ru>
2022-04-23 21:16:21 +0500

    - adaptive nav

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,41 @@
 import { useNavigate } from "react-router-dom";
 import styles from './Header.module.scss';
 import { Context } from '../../index';
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 function Header() {
     const { user } = useContext(Context)
     const history = useNavigate()
+    const [mobileNavVisible, setMobileNavVisible] = useState(false);
+
     return (
         <header className={styles.navbarContainer}>
-            <div className={styles.hamburger}>
+            <div onClick={() => setMobileNavVisible(!mobileNavVisible)} className={styles.hamburger}>
                 <img src="./img/hamburger-menu-icon.svg" alt="" />
             </div>
+            {mobileNavVisible && <div className={styles.mobileNav}>
+                <ul >
+                    <li onClick={() => { history('/'); setMobileNavVisible(false) }}>
+                        Главная
+                    </li>
+                    <li onClick={() => { history('/catalogue'); setMobileNavVisible(false) }}>
+                        Каталог
+                    </li>
+                    <li onClick={() => { history('/gallery'); setMobileNavVisible(false) }}>
+                        Галерея
+                    </li>
+                </ul>
+                <a href="https://vk.com/abdulin_ekb" target="_blank">
+                    <img src="./img/social-vk.svg" alt="vk" />
+                </a>
+                <a href="https://www.instagram.com/ABDULIN_Ekb" target="_blank">
+                    <img src="./img/social-instagram.svg" alt="instagram" />
+                </a>
+                <a href="https://www.youtube.com/channel/UCMffuhoOOYdMYF-8F_7RfHw" target="_blank">
+                    <img src="./img/social-youtube.svg" alt="youtube" />
+                </a>
+                <img onClick={() => { user.isAuth ? history('/admin') : history('/login') }} src="/img/user.svg" alt="user" />
+            </div>}
             <ul className={styles.navbarLeft}>
                 <li onClick={() => history('/')}>
                     Главная
